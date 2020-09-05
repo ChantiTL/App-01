0a60625d12d7db52d7b6f2277715a1fd2549f7cf
lavanya <rama74@gmail.com>
2020-09-05 13:11:42 -0400

    added sop code

--- src/com/chanti/TestClass.java
+++ src/com/chanti/TestClass.java
@@ -5,7 +5,7 @@ public class TestClass {
 	public static void main(String[] args) {
 	
 		System.out.println("Initial setup is done");
-
+		System.out.println("This is line 1 code added by lavanya");
 	}
 
 }
